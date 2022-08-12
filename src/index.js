6ab417780a8a4e72a3d7beac7925bd6d6b835ab0
ashubosh <ashubosh@gmail.com>
2022-08-12 23:14:34 +0530

    mONgodb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {default: mongoose } = require('mongoose')
 const route = require('./routes/route.js');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://Ashutosh1504:jcv5sjFrfdkxeEyL@cluster0.aqhdgzq.mongodb.net/AshutoshSingh1504?retryWrites=true&w=majority"
+,{
+    useNewUrlParser: true
+}
+).then( () => {console.log( "Mongodb is connected")})
+.catch(err => console.log(err))
 app.use('/', route);
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
