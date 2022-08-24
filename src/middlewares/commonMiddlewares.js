9e020643afd3a2e0cad059437b9abf0bf1ec8ce3
ashubosh <ashubosh@gmail.com>
2022-08-24 20:36:20 +0530

    j

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -2,7 +2,7 @@
 const moment = require('moment')
 const mid1 = function (req , res , next ){
 
-    console.log ("inside GLOBAL MW");
+    console.log ("inside GLOBAL MW")
     console.log(moment().format('HH:mm:ss'))
     console.log(req.ip)
     console.log(req.originalUrl)
