b300071aaf45830bbf2a0b73505c2be5dff5c3f9
ashubosh <ashubosh@gmail.com>
2022-08-31 02:28:23 +0530

    assignment-auth3

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,28 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
+//Authentication
+const tokenverify = async function (req, res, next) {
+    let token = req.headers["x-auth-token"]
+    //If no token is present in the request header return error. This means the user is not logged in.
+    if (!token)
+        return res.send({ status: false, msg: "token must be present" });
 
+    let decodedToken = jwt.verify(token, "My Name is Ashutosh Kumar Singh of plutonium batch in FunctionUp")
+    if (!decodedToken) return res.send({ status: false, message: "invalid token" });
+
+    req.userId = decodedToken.userId  //Set an attribute in request object 
+    next();
+};
+
+const checkUser = async function(req,res,next){
+    //Authorisation
+    let verfiyuserID = req.userId
+    let userId = req.params.userId;
+    if (verfiyuserID !== userId) return res.send({ status: false, msg: "User logged is not allowed to modify the requested users data" })
+    
     next()
-}
+    }
+    
+module.exports.tokenverify = tokenverify
+module.exports.checkUser = checkUser
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     mobile: {
         type: String,
-
         required: true
     },
     emailId: String,
@@ -15,7 +14,10 @@ const userSchema = new mongoose.Schema( {
         enum: ["male", "female", "other"]
     },
     age: Number,
-    posts: {type: [], deafult: []}
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('AuthUser', userSchema)
